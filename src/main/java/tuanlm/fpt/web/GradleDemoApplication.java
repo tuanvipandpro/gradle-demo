8c35fcadee47f5117e2a07e0a8d8ab543e1b1e98
TuanLM <tuanvipandpro@gmail.com>
2021-01-25 09:31:43 +0700

    init app

---
+++ src/main/java/tuanlm/fpt/web/GradleDemoApplication.java
@@ -0,0 +1,13 @@
+package tuanlm.fpt.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoApplication.class, args);
+	}
+
+}
